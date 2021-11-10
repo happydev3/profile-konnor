62eab5c3f136f15530d429ba3e15af2ddb46843a
happydev3 <godknight0930@gmail.com>
2021-11-10 07:00:31 -0600

    update

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -230,7 +230,7 @@ function mapInit() {
 
     if ($('#map').length) {
         var options = {
-            center: new google.maps.LatLng(41.0552075,-76.4021468),
+            center: new google.maps.LatLng(29.947380,-93.903950),
             zoom: 8,
             mapTypeControl: true,
             gestureHandling: 'cooperative',
@@ -241,128 +241,12 @@ function mapInit() {
                 position: google.maps.ControlPosition.TOP_LEFT
             },
             scaleControl: false,
-            styles: [{
-                'featureType': 'water',
-                'elementType': 'geometry',
-                'stylers': [{
-                    'color': '#576ee9'
-                }, {
-                    'lightness': 17
-                }]
-            }, {
-                'featureType': 'landscape',
-                'elementType': 'geometry',
-                'stylers': [{
-                    'color': '#f5f5f5'
-                }, {
-                    'lightness': 20
-                }]
-            }, {
-                'featureType': 'road.highway',
-                'elementType': 'geometry.fill',
-                'stylers': [{
-                    'color': '#ffffff'
-                }, {
-                    'lightness': 17
-                }]
-            }, {
-                'featureType': 'road.highway',
-                'elementType': 'geometry.stroke',
-                'stylers': [{
-                    'color': '#ffffff'
-                }, {
-                    'lightness': 29
-                }, {
-                    'weight': 0.2
-                }]
-            }, {
-                'featureType': 'road.arterial',
-                'elementType': 'geometry',
-                'stylers': [{
-                    'color': '#ffffff'
-                }, {
-                    'lightness': 18
-                }]
-            }, {
-                'featureType': 'road.local',
-                'elementType': 'geometry',
-                'stylers': [{
-                    'color': '#ffffff'
-                }, {
-                    'lightness': 18
-                }]
-            }, {
-                'featureType': 'poi',
-                'elementType': 'geometry',
-                'stylers': [{
-                    'color': '#f5f5f5'
-                }, {
-                    'lightness': 21
-                }]
-            }, {
-                'featureType': 'poi.park',
-                'elementType': 'geometry',
-                'stylers': [{
-                    'color': '#d5d5d5'
-                }, {
-                    'lightness': 21
-                }]
-            }, {
-                'elementType': 'labels.text.stroke',
-                'stylers': [{
-                    'visibility': 'on'
-                }, {
-                    'color': '#f8f8f8'
-                }, {
-                    'lightness': 25
-                }]
-            }, {
-                'elementType': 'labels.text.fill',
-                'stylers': [{
-                    'saturation': 36
-                }, {
-                    'color': '#222222'
-                }, {
-                    'lightness': 30
-                }]
-            }, {
-                'elementType': 'labels.icon',
-                'stylers': [{
-                    'visibility': 'off'
-                }]
-            }, {
-                'featureType': 'transit',
-                'elementType': 'geometry',
-                'stylers': [{
-                    'color': '#f5f5f5'
-                }, {
-                    'lightness': 19
-                }]
-            }, {
-                'featureType': 'administrative',
-                'elementType': 'geometry.fill',
-                'stylers': [{
-                    'color': '#fefefe'
-                }, {
-                    'lightness': 10
-                }]
-            }, {
-                'featureType': 'administrative',
-                'elementType': 'geometry.stroke',
-                'stylers': [{
-                    'color': '#fefefe'
-                }, {
-                    'lightness': 17
-                }, {
-                    'weight': 1.2
-                }]
-            }],
         };
         var map = new google.maps.Map(document.getElementById('map'), options);
         var marker1 = new google.maps.Marker({
             position: map.getCenter(),
-            title: 'Akio Mori',
-            icon: 'assets/img/location-map.png',
+            title: 'Address',
+            // icon: 'assets/img/location-map.png',
             animation: google.maps.Animation.BOUNCE
         });
         marker1.setMap(map);
